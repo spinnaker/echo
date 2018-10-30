f8bf1ba2d4f5aea95dbf36a66ece0381c2339fa1
Jacob Kiefer <jacobkiefer@google.com>
2018-10-30 12:18:11 -0400

    fix(MPT): Fixes templated pipeline triggers from Echo. (#383)
    * fix(MPT): Fix templated pipeline triggers. (#370)
    
    * fix(MPT): Fix NPE in pipeline initiator (#371)
    
    * fix(pipelines): Fixes imports for pipeline initiator.
    
    Small backport since this file was recently refactored
    to not use Action1 in master.

--- echo-pipelinetriggers/src/test/groovy/com/netflix/spinnaker/echo/pipelinetriggers/orca/PipelineInitiatorSpec.groovy
+++ echo-pipelinetriggers/src/test/groovy/com/netflix/spinnaker/echo/pipelinetriggers/orca/PipelineInitiatorSpec.groovy
@@ -1,10 +1,10 @@
 package com.netflix.spinnaker.echo.pipelinetriggers.orca
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.netflix.spectator.api.NoopRegistry
 import com.netflix.spinnaker.echo.model.Pipeline
 import com.netflix.spinnaker.fiat.shared.FiatStatus
 import spock.lang.Specification
-import spock.lang.Subject
 import spock.lang.Unroll
 
 import static rx.Observable.empty
@@ -13,13 +13,14 @@ class PipelineInitiatorSpec extends Specification {
   def registry = new NoopRegistry()
   def orca = Mock(OrcaService)
   def fiatStatus = Mock(FiatStatus)
+  def objectMapper = Mock(ObjectMapper)
 
 
   @Unroll
   def "calls orca #orcaCalls times when enabled=#enabled flag"() {
     given:
-    def pipelineInitiator = new PipelineInitiator(registry, orca, fiatStatus, enabled, 5, 5000)
-    def pipeline = Pipeline.builder().application("application").name("name").id("id").build()
+    def pipelineInitiator = new PipelineInitiator(registry, orca, fiatStatus, objectMapper, enabled, 5, 5000)
+    def pipeline = Pipeline.builder().application("application").name("name").id("id").type("pipeline").build()
 
     when:
     pipelineInitiator.call(pipeline)
@@ -32,5 +33,33 @@ class PipelineInitiatorSpec extends Specification {
     enabled || orcaCalls
     true    || 1
     false   || 0
+  }
+
+  @Unroll
+  def "calls orca #orcaCalls to plan pipeline if templated"() {
+    given:
+    def pipelineInitiator = new PipelineInitiator(registry, orca, fiatStatus, objectMapper, true, 5, 5000)
+    def pipeline = Pipeline.builder()
+      .application("application")
+      .name("name")
+      .id("id")
+      .type(type)
+      .build()
+    def pipelineMap = pipeline as Map
+
+    when:
+    pipelineInitiator.call(pipeline)
+
+    then:
+    1 * fiatStatus.isEnabled() >> { return true }
+    orcaCalls * orca.plan(_) >> pipelineMap
+    objectMapper.convertValue(pipelineMap, Pipeline.class) >> pipeline
+    1 * orca.trigger(_) >> empty()
+
+    where:
+    type                || orcaCalls
+    "pipeline"          || 0
+    "templatedPipeline" || 1
+    null                || 0
   }
 }
