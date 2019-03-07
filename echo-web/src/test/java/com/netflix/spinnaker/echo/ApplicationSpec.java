d35d1ae563993d3397d220bc862cae4cc3342b11
Emily Burns <emjeburns@gmail.com>
2019-03-06 16:01:52 -0800

    chore(deps): adding boot2 deps

--- echo-web/src/test/java/com/netflix/spinnaker/echo/ApplicationSpec.java
+++ echo-web/src/test/java/com/netflix/spinnaker/echo/ApplicationSpec.java
@@ -19,13 +19,15 @@ package com.netflix.spinnaker.echo;
 import com.netflix.spinnaker.echo.pipelinetriggers.orca.OrcaService;
 import com.netflix.spinnaker.echo.services.Front50Service;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {Application.class})
 @TestPropertySource(properties = { "spring.config.location=classpath:echo-test.yml" })
 public class ApplicationSpec {
