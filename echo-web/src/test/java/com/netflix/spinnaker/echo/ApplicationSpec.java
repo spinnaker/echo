61e9deec379032cc0eb4a91e1b5ce6ad1f0ce1fa
Emily Burns <emjeburns@gmail.com>
2019-03-12 15:20:42 -0700

    fix(test): change to jupiter

--- echo-web/src/test/java/com/netflix/spinnaker/echo/ApplicationSpec.java
+++ echo-web/src/test/java/com/netflix/spinnaker/echo/ApplicationSpec.java
@@ -18,14 +18,12 @@ package com.netflix.spinnaker.echo;
 
 import com.netflix.spinnaker.echo.pipelinetriggers.orca.OrcaService;
 import com.netflix.spinnaker.echo.services.Front50Service;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {Application.class})
