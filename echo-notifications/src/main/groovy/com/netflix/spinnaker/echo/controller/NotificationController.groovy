83c7d86b6b3fabaa676891398351b8761e9df8fe
Luis Pollo <lpollo@netflix.com>
2020-01-16 12:37:56 -0800

    Document notification callback controller endpoint

--- echo-notifications/src/main/groovy/com/netflix/spinnaker/echo/controller/NotificationController.groovy
+++ echo-notifications/src/main/groovy/com/netflix/spinnaker/echo/controller/NotificationController.groovy
@@ -23,6 +23,7 @@ import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -46,13 +47,25 @@ class NotificationController {
     notificationServices?.find { it.supportsType(notification.notificationType) }?.handle(notification)
   }
 
+  /**
+   * Provides a generic callback API for notification services to call, primarily in response to interactive user
+   * action (e.g. clicking a button in a message). This method makes as few assumptions as possible about the request,
+   * delegating the raw request headers, parameters and body to the corresponding [InteractiveNotificationService]
+   * to process, and similarly allows the notification service to return an arbitrary response body to the caller.
+   *
+   * @param source The unique ID of the calling notification service (e.g. "slack")
+   * @param headers The request headers
+   * @param rawBody The raw body of the request
+   * @param parameters The request parameters, parsed as a Map
+   * @return
+   */
   @RequestMapping(
     value = '/callbacks/{source}',
     method = RequestMethod.POST,
     consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE
   )
-  Optional<String> processCallback(
+  Optional<ResponseEntity<String>> processCallback(
     @PathVariable String source,
     @RequestHeader HttpHeaders headers,
     @RequestBody String rawBody,
